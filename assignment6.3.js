ccda991d1f0995d7558a19c605d936cc75323078
antharamshivakumar <antharamshivakumar@gmail.com>
2016-11-03 12:19:43 -0700

    Add files via upload

---
+++ assignment6.3.js
@@ -0,0 +1,9 @@
+var answer=0;
+var start_value =1;
+var end_value = 1001;
+for (start_value; start_value < end_value; start_value++) {
+    
+    answer = answer + start_value;
+}
+
+console.log("answer= " + answer);
